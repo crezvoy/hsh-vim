e6556fdf0acd4b8f0177ea8ee4cd5ccd11d4ae3e
Clément Rezvoy <clement.rezvoy@gmail.com>
2020-06-24 23:26:29 +0200

    install vim on clone

---
+++ h.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# this script is called after each git command, after 
+# the 'bundle' and 'remove' command which have no git equivalent, and after the
+# extraction of a bundle.
+# The following environment variables are set:
+#     - HSH_ROOT: current hsh root;
+#     - HSH_REPOSITORY: current repository name;
+#     - HSH_ACTION: current git or hsh action, for "bundle" command, HSH_ACTION
+#       is set to 'bundle-in', when extracting a bundle the action is set to
+#       'bundle-out'.
+# Additionally, for bundle-in and bundle-out actions the HSH_BUNDLE_ROOT is set
+# to the bundle content root.
+
+case "$HSH_ACTION" in
+	clone|bundle-out)
+		pk install vim
+		;;
+esac
